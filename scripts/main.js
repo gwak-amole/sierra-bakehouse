362e86a2a159c0ffcd47df3207c577126eefd31c
gwak-amole <gwaksophie@gmail.com>
2025-12-08 20:27:07 -0800

    added custom fonts to make website look better. added a footer for contact info.

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ function openTable(evt, tableName) {
 
     tabcontent = document.getElementsByClassName("menu-grid");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
         tabcontent[i].className = tabcontent[i].className.replace(" active", "")
     }
 
@@ -12,13 +11,12 @@ function openTable(evt, tableName) {
         tablinks[i].className = tablinks[i].className.replace(' active', "");
     }
 
-    document.getElementById(tableName).style.display = "grid";
     document.getElementById(tableName).className += " active";
     
     evt.currentTarget.className += ' active';
 }
 
 function closeTab(closeButton) {
-    closeButton.parentElement.style.display = 'none';
+    closeButton.parentElement.className = closeButton.parentElement.className.replace(" active", "");
     console.log("Close function executed!");
 }
